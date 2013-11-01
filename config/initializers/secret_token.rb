e4450338dbdf588b3585d0e074cb6479f9138772
danphelps <dphelps.umass.edu>
2013-11-01 01:50:59 -0400

    first commit: added status scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magicmaker::Application.config.secret_token = '26a0c93f0843cf4ceffbeb2d058c25c11135ab870ee6ae1bbdca746ffb5dfb91f62c14e1449bdd3f70ff8bdc31273455c48a11e667b82bf480be70c64c9505e8'
